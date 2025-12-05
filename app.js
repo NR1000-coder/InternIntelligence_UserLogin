052085f970cee2dab9beeb17d7dc073938681b4f
Narmin <nrminseyfullayeva@gmail.com>
2025-12-05 21:41:06 +0000

    next

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+$(document).ready(function () {
+    // const Container = document.querySelector('.main-')
+    // const signupLink = document.querySelector('.signup-link')
+    // const loginLink = document.querySelector('.login-link') 
+
+    // ("login-button").onclick = () => {
+    //     Container.classList.add('.active')
+    // }
+    // loginLink.onclick = () => {
+    //    Container.classList.remove('.active') 
+    // }
+});
